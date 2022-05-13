83dc59c3a5547d4f1e9807a70fa527bf190ec282
Binal Lad <binal.lad@1rivet.local>
2022-05-13 19:01:45 +0530

    feat(all files): add angular demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { TicketsModule } from './modules/tickets/tickets.module';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    TicketsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/models/menu.model.ts
@@ -0,0 +1,5 @@
+export interface MenuList {
+  name: string;
+  to: string;
+  iconClass: string;
+}
